d48f5f2bcaec7551cc5c6843ae78b115db2a3e9e
Shalinisinha22 <shalinisinha.cspatna@gmail.com>
2025-06-16 11:59:58 +0530

    routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import authRoutes from './authRoutes';
+
 import roomRoutes from './roomRoutes';
 
 const router = express.Router();
 
-router.use('/auth', authRoutes);
+
 router.use('/rooms', roomRoutes);
 
 export default router;
